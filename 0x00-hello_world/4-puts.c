9d73def2ebfde793abdf6421c0e36cd32dcbb2ff
Francis Chioma <ichiomafrancis@gmail.com>
2022-06-02 16:38:27 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ *
  * Prints a word using puts
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
