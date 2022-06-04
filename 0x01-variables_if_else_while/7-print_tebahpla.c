c9419951255f9ace529f7a6d02b521084225545a
Francis Chioma <ichiomafrancis@gmail.com>
2022-06-04 13:27:27 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
