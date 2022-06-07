f6a32385635545834b551cf2cd3aca0333c6c9a5
Francis Chioma <ichiomafrancis@gmail.com>
2022-06-07 07:00:41 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Program to print _putchar followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	printf("_putchar\n");
+
+
+	return (0);
+}
