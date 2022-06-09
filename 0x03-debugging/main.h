50ef8a6edd35e7001ae583922335adb3b0074029
Francis Chioma <ichiomafrancis@gmail.com>
2022-06-09 15:13:57 -0700

    0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 
 /*0. Multiple mains*/
 void positive_or_negative(int i);
 
 /* 2. 0>972*/
 int largest_number(int a, int b, int c);
+
 
 #endif /* MAIN_H */
