2be6d102a4a89ca6ad769a49c4a450891190d952
Francis Chioma <ichiomafrancis@gmail.com>
2022-06-09 07:02:43 -0700

    Write a function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* 0. isupper*/
+int _isupper(int c);
+
+/* 1. isdigit*/
+int _isdigit(int c);
+
+/* 2. Collaboration is multiplication*/
+int mul(int a, int b);
+
+/* 3. The numbers speak for themselves*/
+void print_numbers(void);
+
+/* 4. I believe in numbers and signs*/
+void print_most_numbers(void);
+
+/* 5. Numbers constitute the only universal language*/
+void more_numbers(void);
+
+/* 6. The shortest distance between two points is a straight line*/
+void print_line(int n);
+
+/* 7. I feel like I am diagonally parked in a parallel universe*/
+void print_diagonal(int n);
+
+/* 8. You are so much sunshine in every square inch*/
+void print_square(int size);
+
+/* 10. Triangles*/
+void print_triangle(int size);
+
+/* 12. Numbers have life; they're not just symbols on paper*/
+void print_number(int n);
+
+
+#endif
