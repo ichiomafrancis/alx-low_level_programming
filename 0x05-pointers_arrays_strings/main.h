b9096a5a3577ee7c4d2d0fe2e1722f876f61ee8a
Francis Chioma <ichiomafrancis@gmail.com>
2022-06-13 15:14:09 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,43 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar function*/
+int _putchar(char c);
+
+/* 0. 98 Battery st.*/
+void reset_to_98(int *n);
+
+/*1. Don't swap horses in crossing a stream */
+void swap_int(int *a, int *b);
+
+/*2*/
+int _strlen(char *s);
+
+/*3. I do not fear computers. I fear the lack of them */
+void _puts(char *str);
+
+/*4. I can only go one way. I've not got a reverse gear */
+void print_rev(char *s);
+
+/*5 */
+void rev_string(char *s);
+
+/*6 */
+void puts2(char *str);
+
+/*7 */
+void puts_half(char *str);
+
+/*8 */
+void print_array(int *a, int n);
+
+/*9 */
+char *_strcpy(char *dest, char *src);
+
+/*10 */
+int _atoi(char *s);
+
+/* 11*/
+
+
+#endif
