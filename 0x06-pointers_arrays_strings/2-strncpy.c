dbb4c49b9db2d570a189b74aeb888e18bcb3d1fd
Francis Chioma <ichiomafrancis@gmail.com>
2022-06-16 11:18:48 -0700

    2. strncpy - a function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,23 +9,19 @@
  *
  * Return: a pointer to the resulting string dest.
  */
-char *_strncopy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
+	int len = 0;
 
-	while (i != n)
-	{
-		dest[j] = src[i];
-		j++;
-		i++;
-		if (src[i] == '\0')
-		{
-			break;
-		}
-	}
-	while (j != n)
-		dest[j++] = '\0';
+	while (src[i++])
+		len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = len; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
