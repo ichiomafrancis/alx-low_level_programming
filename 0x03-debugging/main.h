7c6bfcbc408ea515c987ae61870a0ed530c69bd3
ichiomafrancis <ichiomafrancis@gmail.com>
2023-03-23 07:26:12 -0700

    created the file 3-main_b.c and also updated main.h file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,10 +5,16 @@
 
 void positive_or_negative(int i);
 
+/* 2-largest_number.c */
 int largest_number(int a, int b, int c);
 
+/* 3-main_b.c */
+int main(void)
+
+/* 3-convert_day.c */
 int convert_day(int month, int day);
 
+/* 3-print_remaining_days.c */
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
