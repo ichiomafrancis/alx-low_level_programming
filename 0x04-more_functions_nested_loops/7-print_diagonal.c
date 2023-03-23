f9237c430018b2dd7ceb8ec1bcd904abfa93ab04
ichiomafrancis <ichiomafrancis@gmail.com>
2023-03-23 16:26:32 -0700

    completed task 7. I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @n: The number of \ characters to be printed.
+ */
+void print_diagonal(int n)
+{
+	int len, gap;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (gap = 0; gap < len; gap++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (len == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
+
