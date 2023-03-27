fb4afb25fd12b2f47a4a42701d83c059ab4899fd
ichiomafrancis <ichiomafrancis@gmail.com>
2023-03-27 14:56:29 -0700

    had to reformat my code without using '\n'

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,10 @@ void print_rev(char *s)
 {
 	int i, len = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 		len += 1;
 
-	for (i = len; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
