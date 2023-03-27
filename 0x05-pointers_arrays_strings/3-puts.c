8c1f39f108d81c4b60dc0de54756abdf1fb4b0a3
ichiomafrancis <ichiomafrancis@gmail.com>
2023-03-27 14:02:25 -0700

    completed task 3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string followed
+ *by a new line to stdout.
+ *
+ * @str: the char string to be printed
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
